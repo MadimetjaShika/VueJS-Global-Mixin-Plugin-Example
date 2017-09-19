f1583bd2b5c18f57d53eeee504696e8e5db2dd74
Madimetja Shika <mmjshika@gmail.com>
2017-09-19 22:04:03 +0200

    Added test for file component

--- test/unit/specs/Rilfe.spec.js
+++ test/unit/specs/Rilfe.spec.js
@@ -1,31 +1,31 @@
 import Vue from 'vue';
-import Pistol from '@/components/Pistol/Pistol';
+import Rifle from '@/components/Rifle/Rifle';
 import App from '@/App';
 
-const triggerActionText = 'Arabellum Fired, Yay!';
+const triggerActionText = 'Long Rifle Fired, Whopdie Doo!';
 
-describe('Pistol.vue', () => {
+describe('Rifle.vue', () => {
   it('inits correct default data', () => {
-    expect(typeof Pistol.data).toBe('function');
-    const defaultData = Pistol.data();
+    expect(typeof Rifle.data).toBe('function');
+    const defaultData = Rifle.data();
     expect(defaultData.triggerAction).toBe(triggerActionText);
   });
 
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Pistol);
+    const Constructor = Vue.extend(Rifle);
     const vm = new Constructor().$mount();
 
     expect(vm.$el.querySelector('.title h1').textContent)
-      .to.equal('Pistol');
+      .to.equal('Rifle');
   });
 
   it('trigger pull alerts correct text', () => {
     const Constructor = Vue.extend(App);
     const vm = new Constructor().$mount();
 
-    const button = Pistol.$el.querySelector('button');
+    const button = Rifle.$el.querySelector('button');
     button.dispatchEvent(new window.Event('click'));
-    // Pistol._watcher.run();
+    // Rifle._watcher.run();
 
     expect(vm.$el.querySelector('.notification-text p').textContent).to.contain(triggerActionText);
   });
