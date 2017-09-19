8f36c6392f5703e733466d9d97bf9045076a7072
Madimetja Shika <mmjshika@gmail.com>
2017-09-19 21:07:04 +0200

    Added pistol component skeleton

---
+++ src/components/Pistol/Pistol.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Pistol</h1>
+    <p>Pull Trigger</p>
+    <p>Output</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pistol',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
